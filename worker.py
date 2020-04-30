60fea2a7e11afe187a440b2ccd978c7b5192f5ba
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2020-04-30 23:36:17 +0430

    write worker func addWork()

--- worker.py
+++ worker.py
@@ -1,2 +1,10 @@
 class worker:
-    pass
+    def __init__ (self, name, work = None, workList = {}):
+        self.name = name
+        self.workList = workList
+        self.workList.discard(None)
+
+    def addWork (self, work = None, workList = {}):
+        self.workList.update(workList)
+        self.workList.add(work)
+        self.workList.discard(None)
