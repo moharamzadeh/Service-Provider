347acda18b8058d337b6b24f46ba2b2e5d2304cd
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2020-05-01 00:00:54 +0430

    create and fix static attribute nWork

--- worker.py
+++ worker.py
@@ -1,13 +1,15 @@
 class worker:
-    workList = {}
-    
+    nWork = set()
+
     def __init__ (self, name, work = None, workList = set()):
         self.name = name
         self.workList = workList
+        self.workList.add (work)
         self.workList.discard (None)
-        workList.update (self.workList)
+        worker.nWork.update (self.workList)
 
     def addWork (self, work = None, workList = set()):
         self.workList.update (workList)
         self.workList.add (work)
         self.workList.discard (None)
+        worker.nWork.update (self.workList)
