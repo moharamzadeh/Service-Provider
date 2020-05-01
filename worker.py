f0a9238b5482c4e42dae06e5ec8990188b5bd177
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2020-05-01 05:40:06 +0430

    add each worker, itself works

--- worker.py
+++ worker.py
@@ -1,15 +1,20 @@
 class worker:
-    nWork = set()
+    nWork = []
 
     def __init__ (self, name, work = None, workList = set()):
         self.name = name
         self.workList = workList
         self.workList.add (work)
         self.workList.discard (None)
-        worker.nWork.update (self.workList)
+        # worker.nWork.update (self.workList)
+        personWork = [self.name, self.workList]
+        worker.nWork.append(personWork)
 
     def addWork (self, work = None, workList = set()):
         self.workList.update (workList)
         self.workList.add (work)
         self.workList.discard (None)
-        worker.nWork.update (self.workList)
+        # worker.nWork.update (self.workList)
+        for i in worker.nWork:
+            if i[1] == self.name:
+                i[2].update(self.workList)
