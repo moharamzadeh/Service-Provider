419a130bb98f8079819aef3abdeb2bf92b8fdd64
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2020-05-01 06:02:40 +0430

    delete commented code

--- worker.py
+++ worker.py
@@ -6,18 +6,14 @@ class worker:
         self.workList = dict()
         for work in workList:
             self.workList[work] = 0
-            # self.workList = workList
-        # self.workList[work] = 0
-        # self.workList.discard (None)
+        
         person = [self.name, self.workList]
         worker.nWork.append(person)
 
     def addWork (self, workList = {}):
         for work in workList:
             self.workList[work] = 0
-            # self.workList.update (workList)
-        # self.workList.add (work)
-        # self.workList.discard (None)
+        
         for person in worker.nWork:
             if person[1] == self.name:
                 person[2].update(self.workList)
